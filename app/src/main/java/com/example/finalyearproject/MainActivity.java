3f8d6ad6a07fde4c30139f6ae9478515e616b7cd
Parth Salian <parthsalian19@gmail.com>
2023-03-06 09:25:03 +0530

    This is before adding the algorithm for calculations date is 6th march

---
+++ app/src/main/java/com/example/finalyearproject/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.finalyearproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Setting the title
+        getSupportActionBar().setTitle("Spec Check");
+
+        //Open login activity
+        Button buttonLogin = findViewById(R.id.button_login);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Class<?> LoginActivity;
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        //Open Register Activity
+        Button buttonRegister = findViewById(R.id.button_register);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/finalyearproject/ReadWriteUserDetails.java
@@ -0,0 +1,12 @@
+package com.example.finalyearproject;
+
+public class ReadWriteUserDetails {
+    public String  doB, gender, mobile;
+
+    public ReadWriteUserDetails( String textDoB, String textGender, String textMobile){
+
+        this.doB = textDoB;
+        this.gender = textGender;
+        this.mobile = textMobile;
+    }
+}
